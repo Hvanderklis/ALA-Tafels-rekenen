bd71b84c9aaa21fe11b138199892d097eaceb9d7
Henk Van Der Klis <Henk Van Der Klis>
2016-01-11 22:10:44 +0100

    Updated

--- script/javascript.js
+++ script/javascript.js
@@ -1,3 +1,51 @@
 /**
  * Created by Henk Van Der Klis on 8-12-2015.
  */
+function Submit(){
+    for (x = 0; x < maxTafel; x++){
+        if(parseInt(document.getElementById('antwoord'+x).value) === antwoorden[x]){
+            goed++;
+        }
+        else{
+            fout++;
+        }
+    }
+    grade = ;
+    if (score > 7){
+        alert("goedzo!");
+    }
+    else{
+        alert("volgende keer beter");
+    }
+    document.getElementById('grade').innerHTML = "Je hebt een " + grade + "!";
+}
+function Alles(){
+    /*IMPORTING STUFF*/
+    fout = 0;
+    goed = 0;
+    x = 0;
+    antwoorden = new Array();
+    maxTafelAllowed = 12;
+    maxTafel = 13;
+    /*DONE IMPORTING */
+
+    soortTafel = parseInt(prompt("welke tafel wil je doen?"));
+    if (soortTafel > maxTafelAllowed){
+        alert("Geef een tafel van 0 tot 12 op");
+        Alles();
+    }
+    else if(isNaN(soortTafel)){
+        alert("Geef een tafel van 0 tot 12 op");
+        Alles();
+    }
+
+    function exNumber(soortTafel){
+        for (x = 0; x < maxTafel; x++){
+            antwoorden[x] = soortTafel * x;
+
+            document.getElementById("vragenlijst").innerHTML +="<span id=vraag"+x+">";
+            document.getElementById("vraag"+x).innerHTML = "<p><span>wat is " + soortTafel + " keer " + x + "</span> " + "<input type=text id='antwoord"+x + "'> </input></p>";
+        }
+    }
+    exNumber(soortTafel);
+}
